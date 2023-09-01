37b80aa2d9f8b9f515674ea54b3e3d11e0973260
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-01 15:59:13 +0000

    Add trivia DB using fetch API and loged to console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answers"));
 
+// Trivia Database API
+
+fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple")
+    .then(res => {
+        return res.json();
+    })
+    .then(databaseQuestions => {
+        console.log(databaseQuestions.results);
+    })
+
