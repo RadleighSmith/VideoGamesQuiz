23ae34da17cebad06518688704dd1049dcd01161
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-02 16:53:10 +0000

    Add event listener to return the user to the home page from the instructions page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,23 @@ const answers = Array.from(document.getElementsByClassName("answers"));
 /** Event Listener to show instructions page and hide quiz selection homepage */
 instructionsBtnHomepage.addEventListener("click", function () {
     instructionsBtnHomepage.classList.add("hide");
-    quizHomepageElements.forEach(function (instructions) {
-        instructions.classList.add("hide");
+    quizHomepageElements.forEach(function (homepage) {
+        homepage.classList.add("hide");
     });
 
     instructionsPage.classList.remove("hide");
     instructionBtn.classList.remove("hide");
+});
+
+/** Event Listener to return to quiz selection homepage and hide instructions page */
+instructionBtn.addEventListener("click", function () {
+    instructionsBtnHomepage.classList.remove("hide");
+    instructionsPage.classList.add("hide");
+    instructionBtn.classList.add("hide");
+
+    quizHomepageElements.forEach(function (instructions) {
+        instructions.classList.remove("hide");
+    });
 });
 
 // Trivia Database API
