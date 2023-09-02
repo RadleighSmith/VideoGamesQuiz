1a8b531017d76bbc59ec0e650d2ea52647ebb1c4
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-02 16:53:37 +0000

    remove triva db api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,23 +36,3 @@ instructionBtn.addEventListener("click", function () {
 });
 
 // Trivia Database API
-
-function getQuestions(start) {
-    if (start) {
-        gameUrl = (`https://opentdb.com/api.php?amount=${quest}&category=15&difficulty=${diff}&type=multiple`);
-    } else {
-        gameUrl = ("https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple");
-    }
-
-    console.log("API URL:", gameUrl);
-}
-
-// Retrieves the users options
-
-submit.addEventListener('click', () => {
-    quest = questChoice.options[questChoice.selectedIndex].id;
-    diff = diffChoice.options[diffChoice.selectedIndex].id;
-
-    // Call getQuestions(true) with user-selected options
-    getQuestions(true);
-});
