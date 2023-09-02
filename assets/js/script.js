fecf943f8ca5dbfa7eaf7e2c399cec1ae9750dd3
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-02 14:07:40 +0000

    Fix issue where function was being called immediately

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,24 @@ const submit = document.getElementById("submit");
 const questChoice = document.getElementById("question-choice");
 const diffChoice = document.getElementById("difficulty-choice");
 
+// Trivia Database API
+
+function getQuestions(start) {
+    if (start) {
+        gameUrl = (`https://opentdb.com/api.php?amount=${quest}&category=15&difficulty=${diff}&type=multiple`);
+    } else {
+        gameUrl = ("https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple");
+    }
+
+    console.log("API URL:", gameUrl);
+}
+
 // Retrieves the users options
 
 submit.addEventListener('click', () => {
     quest = questChoice.options[questChoice.selectedIndex].id;
     diff = diffChoice.options[diffChoice.selectedIndex].id;
+
+    // Call getQuestions(true) with user-selected options
+    getQuestions(true);
 });
-
-// Trivia Database API
-
-fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple")
-    .then(res => {
-        return res.json();
-    })
-    .then(databaseQuestions => {
-        console.log(databaseQuestions.results);
-        databaseQuestions.results.map(databaseQuestions => {
-            const displayQuestion = {
-                question: databaseQuestions.question
-            };
-            console.log(displayQuestion);
-        });
-    });
-
