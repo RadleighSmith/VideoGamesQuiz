f29871e60ff7bb18c34456fc4e92e1b1f58d0302
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-03 15:25:42 +0000

    Add click event listeners to show and hide instructions page

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,36 @@
 const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answers"));
+const instructionsBtnHomepage = document.getElementById("intructions-btn-homepage");
+const quizHomepageElements = document.querySelectorAll(".quiz-homepage");
+const instructionsPage = document.getElementById("instruction-page");
+const instructionBtn = document.getElementById("instruction-btn");
+
 
 let currentQuestion = {};
 let questionCounter = 0;
 let correctCount = 0;
 let incorrectCount = 0;
 let acceptingAnswers = true;
+
+
+/** Event Listener to show instructions page and hide quiz selection homepage */
+instructionsBtnHomepage.addEventListener("click", function () {
+    instructionsBtnHomepage.classList.add("hide");
+    quizHomepageElements.forEach(function (homepage) {
+        homepage.classList.add("hide");
+    });
+
+    instructionsPage.classList.remove("hide");
+    instructionBtn.classList.remove("hide");
+});
+
+/** Event Listener to return to quiz selection homepage and hide instructions page */
+instructionBtn.addEventListener("click", function () {
+    instructionsBtnHomepage.classList.remove("hide");
+    instructionsPage.classList.add("hide");
+    instructionBtn.classList.add("hide");
+
+    quizHomepageElements.forEach(function (instructions) {
+        instructions.classList.remove("hide");
+    });
+});
