3d470de813cbaa5a67d756dc2551ab5a8042a651
Radleigh Smith <135361802+RadleighSmith@users.noreply.github.com>
2023-09-03 15:21:51 +0000

    Add variables for the HUD

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,8 @@
 const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answers"));
+
+let currentQuestion = {};
+let questionCounter = 0;
+let correctCount = 0;
+let incorrectCount = 0;
+let acceptingAnswers = true;
